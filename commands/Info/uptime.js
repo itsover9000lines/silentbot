09ea542950c344198dfb66d3b283dbae146a17b7
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-17 19:33:37 -0400

    Update uptime.js

--- commands/Info/uptime.js
+++ commands/Info/uptime.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
     let uptime = (`${hours} Hours\n${minutes} Minutes\n${seconds} Seconds`);
         const embed = new Discord.RichEmbed()
         .setColor(`#FF000`)
-        .addField(`Uptime`, ${duration}, true)
+        .addField(`Uptime`, uptime, true, Math.round(number))
         .setFooter(`Command Ran By: ${message.author.username}`, message.author.avatarURL)
     message.channel.send(embed);
 }
