ec6aa40b59dfc86caedac3341017b3146fc35637
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-17 09:55:10 -0400

    Update report.js

--- commands/Moderation/report.js
+++ commands/Moderation/report.js
@@ -32,7 +32,6 @@ module.exports.run = async (bot, message, args) => {
 
 
     let reportschannel = message.guild.channels.find(`name`, "silent-log");
-    let reportschannel = message.guild.channels.find(`name`, "bot-spam");
     if (!reportschannel) return message.channel.send("Couldn't find silent-log.");
 
     message.delete().catch();
