8fd80fb4208c67881d4a1ddfb568650799b819aa
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-17 19:42:50 -0400

    Update uptime.js

--- commands/Info/uptime.js
+++ commands/Info/uptime.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const ms = require('ms');
 module.exports.run = async (bot, message, args) => {
     let totalSeconds = (bot.uptime / 1000);
-    let hours = Math.floor(totalSeconds / 3600);
+    let hours = Math.round(totalSeconds / 3600);
     totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
+    let minutes = Math.round(totalSeconds / 60);
     let seconds = Math.round(totalSeconds % 60);
     let uptime = (`${hours} Hours\n${minutes} Minutes\n${seconds} Seconds`);
         const embed = new Discord.RichEmbed()
