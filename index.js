c413a8029c7bd4c51f2ef76fd63ea1cb5d302f7a
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-18 10:46:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ bot.on('messageDelete', async (message) => {
     let logembed = new Discord.RichEmbed()
         //.setTitle('Message Deleted')
         .setAuthor(user.tag, message.author.displayAvatarURL)
-        .addField(`Message sent by <@${message.auhor.id}> deleted in <#${message.channel.id}>\n\n`, message.content)
+        .addField(`Message sent by <@${message.author.id}> deleted in <#${message.channel.id}>\n\n`, message.content)
         .setColor("#FF0000")
         .setFooter(`<#${message.channel.id}>`)
         .setTimestamp()
