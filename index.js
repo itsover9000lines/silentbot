b1fe4f9d5507176b0e04e20d450cc8aeb2bc9056
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-18 09:52:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,34 @@ bot.on("guildMemberAdd", async member => {
         .setThumbnail(member.user.avatarURL)
     await modlogs.send(botembed);
 });
+
+bot.on('messageDelete', async (message) => {
+    const logs = message.guild.channels.find('name', 'logs');
+    if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+        await message.guild.createChannel('logs', 'text');
+    }
+    if (!logs) {
+        return console.log('The logs channel does not exist and cannot be created')
+    }
+    const entry = await message.guild.fetchAuditLogs({
+        type: 'MESSAGE_DELETE'
+    }).then(audit => audit.entries.first())
+    let user;
+    if (entry.extra.channel.id === message.channel.id && (entry.target.id === message.author.id) && (entry.createdTimestamp > (Date.now() - 5000)) && (entry.extra.count >= 1)) {
+        user = entry.executor.username
+    } else {
+        user = message.author
+    }
+    const logembed = new Discord.RichEmbed()
+        .setTitle('Message Deleted')
+        .setAuthor(user.tag, message.author.displayAvatarURL)
+        .addField(`**Message sent by ${message.author.username}> deleted in ${message.channel.name}**\n\n`, message.content)
+        .setColor(message.guild.member(client.user).displayHexColor)
+        .setFooter(`<#${message.channel.id}>`)
+        .setTimestamp()
+    //console.log(entry)
+    logs.send(logembed);
+})
 bot.on("guildMemberRemove", async member => {
     member.send(`Hope you enjoyed your time here in ${member.guild.name} ${member}`)
     let guild = member.guild;
