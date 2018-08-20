ce88d8758d93d800cc14a5f082b055759a1be9fc
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-20 19:25:35 -0400

    Update supportdisc.js

--- commands/Info/supportdisc.js
+++ commands/Info/supportdisc.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
         .setColor("#7EC0EE")
-        .addField(`Support`, `[Suppor Discord](https://discord.gg/8dTVWrg)`)
+        .addField(`Support`, `[Support Discord](https://discord.gg/8dTVWrg)`)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
