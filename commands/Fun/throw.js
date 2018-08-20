24f7e7f698ab821b0ae3dccd1795de8baa7ca86a
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-20 19:09:38 -0400

    Update throw.js

--- commands/Fun/throw.js
+++ commands/Fun/throw.js
@@ -1,7 +1,12 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     const throwuser = message.mentions.users.first() || message.author;
-    let replies = [`${message.author.tag}`,
+    let replies = [`Banana`, `Car`, `Truck`, `Hot Firemen`, `A Building`, 
+    `SpongeBob`, 'Patrick', 'Nothing', 'Admins', 'Moderators', 
+    `Black Hole`, `Scams` , `Love`, `Hate`, `iPhone`, `Brick`, `Bad Bots`, `Chair`, 
+    `Lemons`, `Cake`, `Pringles`, `Gummy Bears`, `Bus`, `Train`, `Yourself`, `Knife`, `UR MOM`,
+    `Self Bots`, `A big hug`, `A teddy bear`, `Work`, `My love and effection`, `Memes`, `Happiness`,
+    `Money`, `Poverty`, `A fan`, `${message.author.tag}`,
 ]
     let result = Math.floor((Math.random() * replies.length));
     let embed = new Discord.RichEmbed()
