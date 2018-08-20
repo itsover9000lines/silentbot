bbea0ef8eaf9983c58a58be9e626e5f99bd2163e
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-20 18:56:05 -0400

    Update avatar.js

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let aTaged = message.mentions.users.first();
     if (!aTaged) {
-        return message.channel.send(`Please Provide a User <a:wink22:461162773787836437>` + "\n **s!avatar @user**");
+        return message.channel.send(`Please tag a user!` + "\n **s!avatar @user**");
     }
     let botembed = new Discord.RichEmbed()
         .setColor("#000FF")
