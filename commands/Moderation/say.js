e86ab55a93d251f6c0e45dc93f7b3c1619bdfedc
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-20 16:21:15 -0400

    Update say.js

--- commands/Moderation/say.js
+++ commands/Moderation/say.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry but you don't have the permissions to use this command.");
+    if (message.author.id !== "20325989474330214") return message.reply("Sorry but you don't have the permissions to use this command.");
     let botmessage = args.join(" ");
     message.channel.send(botmessage);
     message.delete().catch();
