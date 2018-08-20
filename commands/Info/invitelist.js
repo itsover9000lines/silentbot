3a6dbbffa6e65c162893702a9b6cf29805cb259a
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-20 19:50:48 -0400

    Update invitelist.js

--- commands/Info/invitelist.js
+++ commands/Info/invitelist.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let invites = await message.guild.fetchInvites().catch(error => {
-        return message.channel.send('Sorry, I don\'t have the proper permissions to view invites!');
+        return message.channel.send('Sorry, I don\'t have the permissions to view invites!');
     });
 
     invites = invites.array();
