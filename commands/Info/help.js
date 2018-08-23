170a2a08d81e391fbef7b97d784b44bf2da92ba9
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-08-23 16:48:23 -0400

    Update help.js

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
         .setColor("#7EC0EE")
-        .addField(`Commands List`, `[Parteon](https://www.patreon.com/SilenGamer) \n [Moderation](https://hastebin.com/ejelacuzux.js) \n [Info](https://hastebin.com/efesokulom.sql) \n [Fun](https://hastebin.com/viqolapiso.makefile)`)
+        .addField(`Commands List`, `[Parteon](http://bit.ly/2o7Tvxr) \n [Moderation](https://hastebin.com/ejelacuzux.js) \n [Info](https://hastebin.com/efesokulom.sql) \n [Fun](https://hastebin.com/viqolapiso.makefile)`)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
