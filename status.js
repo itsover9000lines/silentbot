ebc206706a4ce581eb450a2f054b5798d83badbf
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-01 16:32:31 -0400

    Create status.js

---
+++ status.js
@@ -0,0 +1,15 @@
+module.exports = (bot) => {
+	bot.user.setActivity(`Prefix e! or E!`, { type: "STREAMING", url: "https://www.twitch.tv/silentgamerplayz" });
+	let userssize = bot.users.size;
+	let channelsize = bot.channels.size;
+	let guildsize = bot.guilds.size;
+	var status = [
+		`s!help | Users: ${userssize},
+		`s!help | Servers: ${guildsize}`,
+    `s!help | Channels: ${channelsize}`
+	];
+	setInterval(() => {
+		let gameval = Math.floor((Math.random() * status.length));
+		bot.user.setActivity(`${status[gameval]}`, { type: "STREAMING", url: "https://www.twitch.tv/silentgamerplayz" });
+	}, 10000);
+};
