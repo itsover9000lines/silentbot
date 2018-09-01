413290bfb184a12043d001f4a7f3c44bf6573fc9
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-01 15:22:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ bot.on("guildMemberAdd", async member => {
 });
 bot.on(`messageDelete`, message => {
     if (message.author.bot) return;
-    let modlogs = message.guild.channels.find('name', 'silent-log');
+    let modlogs = message.guild.channels.find(c => c.name === "silent-log");
     if (!modlogs) return;
     let botembed = new Discord.RichEmbed()
         .setColor("#FF0000")
