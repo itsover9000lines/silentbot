705f34c3c96e515c90fd8a5a959cce6445f48a34
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-06 18:22:57 -0400

    Create nsfw.js

---
+++ commands/Fun/nsfw.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+module.exports.run = async (bot, message, args) => {
+    if(!message.channel.nsfw) return;
+    const subreddits = [
+        "nsfw_hd",
+        "HighResNSFW"
+    ]
+
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setColor(`#20C3FF`)
+                .setDescription(`Getting ur meme`)
+            message.channel.send(embed).then(message => {
+                embed.setColor(`#20C3FF`)
+                embed.setDescription(`heres ur meme lulxd [Click Here](${url})`)
+                embed.setImage(url)
+                message.edit(embed);
+            })
+        })
+}
+module.exports.help = {
+    name: "nsfw",
+    names: "Nsfw"
+}
