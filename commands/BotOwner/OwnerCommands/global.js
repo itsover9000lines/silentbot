cf41098ffcdeb1ad94e84c5fa39486009965afe1
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-06 15:42:55 -0400

    Update global.js

--- commands/BotOwner/OwnerCommands/global.js
+++ commands/BotOwner/OwnerCommands/global.js
@@ -3,14 +3,14 @@ const Discord = require('discord.js');
 if (message.content.startsWith(prefix + "global")) {
     let xoargs = message.content.split(" ").slice(1);
     let xo03 = xoargs.join(" ")
-    var xo02 = message.guild.channels.find('name', 'Channel name');
-    if (!xo02) return message.reply(`the channel "channel name" does not exist!`)
-    if (message.channel.name !== 'redtchat') return message.reply('command to be made in the "channel name "')
+    var xo02 = message.guild.channels.find('name', 'silent-log');
+    if (!xo02) return message.reply(`The channel "silent-log" does not exist!`)
+    if (message.author.id !== "203259894743302145") return message.reply('You are not allowed to use this command.')
                 if (!xo03) return message.reply('You must write a message')
                     message.delete();
                     var tchat_embed = new Discord.RichEmbed()
                         .setColor("#677BC4")
-                        .setTitle("RedChat Global")
+                        .setTitle("Important Message From Silent")
                         .setThumbnail(`${message.author.displayAvatarURL}`)
                         .addField("Name of user:", message.author.username + "#" + message.author.discriminator, true)
                         .addField("From server", message.guild.name, true)
