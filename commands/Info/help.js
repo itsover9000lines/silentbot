dab9ac2d3de9eb574166d0825b02ccb992c66064
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-10 10:12:47 -0400

    Reacts with ✅

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    
+    await message.react("✅");
+    
     let botembed = new Discord.RichEmbed()
         .setColor("#20C3FF")
         .addField(`Commands List`, `[Patreon](http://bit.ly/2o7Tvxr) \n [Moderation](https://hastebin.com/ejelacuzux.js) \n [Info](https://hastebin.com/efesokulom.sql) \n [Fun](https://hastebin.com/viqolapiso.makefile)`)
