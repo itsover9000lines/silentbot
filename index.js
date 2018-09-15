9be52012862f115ed9fc672a39a31c081722a0f4
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-15 18:57:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,34 @@ bot.on("ready", async () => {
     console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 });
 
+ const ownerID = '203259894743302145';
+
+
+     if (command === "leaveserver") {
+         if (message.author.id !== ownerID) return message.channel.send("You are not authorized to use this command.");
+
+         var error17 = new Discord.RichEmbed().setColor("990033")
+             .setDescription('**Please enter a valid server ID.**')
+             .setColor(0xff0000)
+
+         var error18 = new Discord.RichEmbed().setColor("990033")
+             .setDescription('**You cannot kick the bot from this server!**')
+             .setColor(0xff0000)
+
+
+         if (isNaN(args[0])) return message.channel.send(error17).then(msg => {
+             msg.delete(9000)
+         });
+
+         //If tried kick bot from a main server (like for emote provider) will return error18
+         if (args[0] !== 373950345153609729 || 481663437705838602) return message.channel.send(error18).then(msg => {
+             msg.delete(9000)
+         });
+
+         bot.guilds.get(args[0]).leave();
+         message.channel.send(`**Bot was been removed from server id [${args[0]}]**`)
+     }
+
 bot.on("guildMemberAdd", async member => {
     //member.send(`Welcome to **${member.guild.name}** ${member}`)
     let serverSize = member.guild.memberCount;
