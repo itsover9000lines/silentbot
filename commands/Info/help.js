31049063d121a4feeb7c7bed9387813f1cf74f9b
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-16 10:43:09 -0400

    Added youtube and twitch ads for me bc lul

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -7,6 +7,8 @@ module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
         .setColor("#20C3FF")
         .addField(`Commands List`, `[Patreon](http://bit.ly/2o7Tvxr) \n [Moderation](https://hastebin.com/ejelacuzux.js) \n [Info](https://hastebin.com/efesokulom.sql) \n [Fun](https://hastebin.com/viqolapiso.makefile)`)
+        .addField(`Subscribe to the creator on YouTube!`, `[Subscribe](http://bit.ly/aljsdkla)`)
+        .addField(`Follow him on twitch!`, `[Follow](http://bit.ly/2QysMqZ)`)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
