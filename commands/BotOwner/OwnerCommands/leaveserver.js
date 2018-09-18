a143e2a4d3bda7a7d3435ad13d2bf7c0c4102e92
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-18 14:33:04 -0400

    Update leaveserver.js

--- commands/BotOwner/OwnerCommands/leaveserver.js
+++ commands/BotOwner/OwnerCommands/leaveserver.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     if (message.author.id !== "203259894743302145") return bot.users.get("203259894743302145").send(`${message.author.username} Has Tried to use the Leave Server Command.`).catch(() => console.error('I couldnt send the PM to the user.'));
-    bot.guilds.find('id', `${args[0]}`).owner.send(`This bot has been removed from your server by the bot owner. For more information please contact the owner, SilentMemer#7124!`)
+    bot.guilds.find('id', `${args[0]}`).owner.send(`The Bot has been removed from your server by the Bot Owner, for more information please contact (Mo_sie#9685)`)
     await bot.guilds.find('id', `${args[0]}`).leave()
 }
 module.exports.help = {
-    name: "leaveserver"
+    name: "leave"
 }
