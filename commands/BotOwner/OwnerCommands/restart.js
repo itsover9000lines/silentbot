1cb35e6479e1927a8c756a2ebb6df19085502425
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-18 14:10:55 -0400

    Update restart.js

--- commands/BotOwner/OwnerCommands/restart.js
+++ commands/BotOwner/OwnerCommands/restart.js
@@ -1,22 +1,13 @@
+
 const Discord = require("discord.js");
 const fs = require('fs');
 
 module.exports.run = async (bot, message, args) => {
-    let botembed = new Discord.RichEmbed()
-    .setColor("#000FF")
-    .addField("Restart Issued", `<@${message.author.id}> Has Restarted the Bot!`)
-    .setThumbnail(bot.user.avatarURL)
-    .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
-    .setTimestamp()
     console.log(`Bot Has Been Restart By: ` + message.author.username);
-    if (message.author.id !== "203259894743302145") return;
+    if (message.author.id !== "203259894743302145") return message.channel.reply("You can't do this!");
     await message.react("✅");
-    await message.channel.send(botembed);
     message.delete(1000).catch();
-    fs.writeFile('./log.json', JSON.stringify(`Bot has Been Restarted.`), (err) => {
-        if (err) console.log(err)
-    });
-    process.exit();
+    await bot.destroy().then(bot.login(process.env.BOT_TOKEN))
 
 }
 module.exports.help = {
