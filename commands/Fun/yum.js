48f391b54792f076d71a7e5b483935ac4ad11ca5
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-09-20 13:15:37 -0400

    Update yum.js

--- commands/Fun/yum.js
+++ commands/Fun/yum.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
                 .setDescription(`Getting ur meme`)
             message.channel.send(embed).then(message => {
                 embed.setColor(`#20C3FF`)
-                embed.setDescription(`woah thats hot [Click Here](${url})`)
+                embed.setDescription(`yumm [Click Here](${url})`)
                 embed.setImage(url)
                 message.edit(embed);
             })
