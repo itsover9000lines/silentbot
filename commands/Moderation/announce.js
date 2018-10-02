1691b3c15c0a22846731e02a6600c410730eb20e
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-10-02 13:13:46 -0400

    Update announce.js

--- commands/Moderation/announce.js
+++ commands/Moderation/announce.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_SERVER")) return;
 let sIcon = message.guild.iconURL;
-let change = message.guild.channels.find(c => c.name === "discord-updates") || message.guild.channels.find(c => c.name === "updates" || message.guild.channel.find(c => c.name === "🔎discord-updates🔍" || message.guild.channel.find(c => c.name === "change-log" || message.guild.channel.find(c => c.name === "changelog" || message.guild.channel.find(c => c.name === "changes")
+let change = message.guild.channels.find(c => c.name === "discord-updates") || message.guild.channels.find(c => c.name === "updates") || message.guild.channel.find(c => c.name === "🔎discord-updates🔍") || message.guild.channel.find(c => c.name === "change-log") || message.guild.channel.find(c => c.name === "changelog") || message.guild.channel.find(c => c.name === "changes")
 let botembed = new Discord.RichEmbed()
     .setColor(`#FF000`)
     .setDescription(`${args[0]} Update \n\n ${args.join(' ').slice(args[0])}`)
