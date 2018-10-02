3ab378d59774e46f09cfacc1df036ae07b33a761
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-10-02 13:34:03 -0400

    Update announce.js

--- commands/Moderation/announce.js
+++ commands/Moderation/announce.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_SERVER")) return;
 let sIcon = message.guild.iconURL;
-//let change = message.guild.channels.find(c => c.name === "discord-updates") || message.guild.channels.find(c => c.name === "updates") || message.guild.channel.find(c => c.name === "🔎discord-updates🔍") || message.guild.channel.find(c => c.name === "change-log") || message.guild.channel.find(c => c.name === "changelog") || message.guild.channel.find(c => c.name === "changes")
 let botembed = new Discord.RichEmbed()
     .setColor(`#FF000`)
     .setDescription(`${args[0]} Update \n\n ${args.join(' ').slice(args[0])}`)
@@ -13,6 +12,5 @@ message.channel.send(botembed);
 message.delete().catch();
 }
 module.exports.help = { 
-    name: "change",
-    names: "announce"
+    name: "announce"
 }
