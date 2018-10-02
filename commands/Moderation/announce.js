5c3a8d234d9a45f346bffc59aaf208ab0eff9b92
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-10-02 13:24:56 -0400

    Update announce.js

--- commands/Moderation/announce.js
+++ commands/Moderation/announce.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_SERVER")) return;
 let sIcon = message.guild.iconURL;
-let change = message.guild.channels.find(c => c.name === "discord-updates") || message.guild.channels.find(c => c.name === "updates") || message.guild.channel.find(c => c.name === "🔎discord-updates🔍") || message.guild.channel.find(c => c.name === "change-log") || message.guild.channel.find(c => c.name === "changelog") || message.guild.channel.find(c => c.name === "changes")
+//let change = message.guild.channels.find(c => c.name === "discord-updates") || message.guild.channels.find(c => c.name === "updates") || message.guild.channel.find(c => c.name === "🔎discord-updates🔍") || message.guild.channel.find(c => c.name === "change-log") || message.guild.channel.find(c => c.name === "changelog") || message.guild.channel.find(c => c.name === "changes")
 let botembed = new Discord.RichEmbed()
     .setColor(`#FF000`)
     .setDescription(`${args[0]} Update \n\n ${args.join(' ').slice(args[0])}`)
     .setThumbnail(sIcon)
     .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`)
     .setFooter(`${message.author.username}`, message.author.avatarURL)
-change.send(botembed);
+message.channel.send(botembed);
 message.delete().catch();
 }
 module.exports.help = { 
