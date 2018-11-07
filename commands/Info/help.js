816b399f973f4de7aa34bb28aacb23d207abe01e
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-11-06 19:50:42 -0500

    Update help.js

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
     
     let botembed = new Discord.RichEmbed()
         .setColor("#20C3FF")
+        .addField(`Website`, `[Link](https://silentbot.tk/)`)
         .addField(`Commands List`, `[Patreon](http://bit.ly/2o7Tvxr) \n [Commands](http://bit.ly/2xnFH6r)`)
         .addField(`Subscribe to the creator on YouTube!`, `[Subscribe](http://bit.ly/aljsdkla)`)
         .addField(`Follow him on twitch!`, `[Follow](http://bit.ly/2QysMqZ)`)
