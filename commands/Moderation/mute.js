4a478735037a6970a6da68f80a337d0d01d87058
itsover9000lines <42451605+itsover9000lines@users.noreply.github.com>
2018-11-30 12:57:33 -0500

    Update mute.js

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -1,79 +1,99 @@
 const { Command } = require('discord.js-commando'),
-    Discord = require('discord.js');
-const ms = require("ms");
+    ms = require('ms')
+Discord = require('discord.js');
 module.exports = class NCommand extends Command {
     constructor(client) {
         super(client, {
             name: "mute",
             memberName: "mute",
-            aliases: [],
-            examples: [`${client.commandPrefix}mute`],
-            description: "Mutes mentioned user. [BROKEN]",
+            aliases: ["silence", "calm"],
+            examples: [`${client.commandPrefix}mute @user/userid <Time Here> <reason here>`],
+            description: "Mutes mentioned member.",
+            guildOnly: true,
+            userPermissions: ["MANAGE_MESSAGES", "MANAGE_GUILD"],
             group: "moderation",
             args: [
                 {
-                    key: 'user',
-                    prompt: 'What user would you like to mute?',
-                    type: 'member'
+                    key: "member",
+                    prompt: "What member do you want me to mute?",
+                    type: "member"
                 },
                 {
-                    key: 'time',
-                    prompt: 'How long is this mute for?',
-                    type: 'string'
+                    key: "time",
+                    prompt: "how much time do you want to mute the user for?",
+                    type: "string"
                 },
                 {
-                    key: 'reason',
-                    prompt: 'What is the reason for this mute?',
+                    key: 'content',
+                    prompt: 'What is the reason for the mute?',
                     type: 'string'
                 }
             ]
         })
     }
-    async run(message, {user, time, reason}) {
-    let bot = this.client
-    const modlogs = message.guild.channels.find(c => c.name === "silent-log") || message.guild.channels.find(c => c.name === "bot-spam")
-    if(!modlogs) return message.channel.send("Can't find **silent-log**");
-    let tomute = user
-    if (!tomute) return message.reply("Couldn't find user.");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
-    let muterole = message.guild.roles.find(`name`, `Muted [${bot.user.username}]`);
-    if (!muterole) {
-        try {
-            muterole = await message.guild.createRole({
-                name: `Muted Role [${bot.user.username}]`,
-                color: "#000000",
-                permissions: []
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(muterole, {
-                    SEND_MESSAGES: false,
-                    READ_MESSAGES: true,
-                    ADD_REACTIONS: false
+    async run(message, { member, time, content }) {
+        const modlogs = message.guild.channels.find(c => c.name === "modlogs")
+        let moderatorname = `<@${message.author.id}>`
+        if (!modlogs) return message.channel.send("Can't find **modlogs**");
+        let tomute = member
+        if (tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry but i can't mute Mods/Admins!");
+        let muterole = message.guild.roles.find(r => r.name === "Muted") || message.guild.roles.find(r => r.name === "muted")
+        let reason = content
+        if (!muterole) {
+            try {
+                muterole = await message.guild.createRole({
+                    name: `Muted`,
+                    color: "#FF0000",
+                    permissions: []
+                })
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions(muterole, {
+                        SEND_MESSAGES: false,
+                        READ_MESSAGES: false,
+                        ADD_REACTIONS: false
+                    });
                 });
-            });
-        } catch (e) {
-            console.log(e.stack);
+            } catch (e) {
+                console.log(e.stack);
+            }
         }
-    }
-    let mutetime = time;
-    if (!mutetime) return message.reply("You didn't specify a time!");
+        let mutetime = time;
 
-    await (tomute.addRole(muterole.id));
-    let botembed = new Discord.RichEmbed()
-    .setColor("#000FF")
-    .setAuthor(`${tomute.user.username}`, `${tomute.user.avatarURL}`)
-    .setDescription(`✅${tomute.user.tag} Has been Muted!`)
-    .addField("Moderator", message.author.username, true)
-    .addField("User Muted",`${tomute.user.tag}`, true)
-    .addField("Time", `${ms(ms(mutetime))}`)
-    .setTimestamp()
-    .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
-    modlogs.send(botembed);
+        await (tomute.addRole(muterole.id));
+        let botembed = new Discord.RichEmbed()
+            .setColor("#000FF")
+            .setAuthor(`${tomute.user.username}`, `${tomute.user.avatarURL}`)
+            .setDescription(`Mute | ${tomute.user.tag} `)
+            .addField("Moderator", moderatorname, true)
+            .addField("User Muted", `<@${tomute.id}>`, true)
+            .addField("Time", `${ms(ms(mutetime))}`, true)
+            .addField(`Reason`, `${reason}`)
+            .setTimestamp()
+            .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
+        modlogs.send(botembed)
+        message.channel.send(`✅ ***${tomute.user.tag} Has Been Muted!***`);
 
-    setTimeout(function () {
-        tomute.removeRole(muterole.id);
-        message.channel.send(`${tomute.user.tag} has been unmuted!`);
-    }, ms(mutetime));
-    message.delete().catch();
+        setTimeout(function () {
+            let unmuteembed = new Discord.RichEmbed()
+                .setColor(`#FF000`)
+                .setDescription(`Unmute | ${tomute.user.tag}`)
+                .addField(`Moderator`, `${moderatorname}`, true)
+                .addField(`User Unmuted`, `<@${tomute.id}>`, true)
+                .addField(`Reason`, `Auto`, true)
+                .setTimestamp()
+                .setFooter(`ID: ${tomute.id}`, `${tomute.user.avatarURL}`)
+            tomute.removeRole(muterole.id);
+            modlogs.send(unmuteembed);
+        }, ms(mutetime));
+        message.delete().catch();
+
+        const dmembed = new Discord.RichEmbed()
+            .setColor(`#FF0000`)
+            .setDescription(`You have been Muted in **${message.guild.name}**`)
+            .addField(`Time`, `${ms(ms(mutetime))}`)
+            .addField(`Reason`, `${reason}`)
+        tomute.send(`<@${tomute.id}>`)
+        await tomute.send(dmembed)
+
     }
 }
